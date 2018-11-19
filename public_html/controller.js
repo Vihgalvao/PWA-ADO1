177c2365c118e9a9c802cfa734943d11fd7e64bd
victor.galvao <victor.galvao@galvao-note.creditcash.com.br>
2018-11-19 18:49:18 -0200

    Criação da lista de tarefas SPA.

---
+++ public_html/controller.js
@@ -0,0 +1,86 @@
+var app = angular.module('TarefasApp', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state('principal', {
+        url: '/',
+        templateUrl: 'views/principal.html'
+    });
+
+    $stateProvider.state('novaTarefa', {
+        url: '/',
+        templateUrl: 'views/novatarefa.html'
+    });
+
+    $urlRouterProvider.otherwise('/');
+
+
+});
+
+app.controller('PrincipalCtrl', function ($scope, $tarefaDao) {
+
+    $scope.tarefas = $tarefaDao.getTarefas();
+    $scope.mostrarTerminadas = true;
+    
+    $tarefaDao.getTarefas().then(function (tarefas) {
+        $scope.tarefas = tarefas;
+    });
+    
+    $scope.filtroTarefas = function(tarefa) {
+        if (tarefa.status == true && $scope.mostrarTerminadas == false) {
+            return false;
+        }
+        return true;
+    };
+    
+    $scope.filtrarTarefasTerminadas = function () {
+        $tarefaDao.filtroTarefas();
+    };
+
+});
+
+//app.controller('TarefaCtrl', function ($scope, $tarefaDao, $stateParams) {
+//
+//    $scope.produto = $tarefaDao.getTarefaById($stateParams.id);
+//
+//});
+
+
+app.factory('$tarefaDao', function ($http, $q) {
+
+    var tarefas = [];
+    
+    function getTarefas() {
+        return $q(function (resolve, reject) {
+            $http.get('tarefas.json').then(function (response) {
+                tarefas = response.data;
+                resolve(tarefas);
+            });
+        });
+    }
+
+    function getTarefaById(id) {
+        for (var i = 0; i < tarefas.length; i++) {
+            var tarefa = tarefas[i];
+            if (tarefa.id == id) {
+                return tarefa;
+            }
+        }
+        return null;
+    }
+
+    return {
+        getTarefas,
+        getTarefaById
+    };
+
+
+    function removeTarefasTerminadas() {
+        var temp = tarefas.filter((tarefa) => {
+            return (!tarefa.status);
+        });
+        tarefas = temp;
+
+    }
+
+});
